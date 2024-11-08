e396c9e5dc740cb2dfab33424e76b1fe5273e4dc
Diya Das <dasdiya0603@gmail.com>
2024-11-08 12:18:07 +0530

    added graphs

--- src/components/Visual.jsx
+++ src/components/Visual.jsx
@@ -1,29 +1,37 @@
 import React from 'react'
 import cancerInformation from './info'
-/*
-    There will be 5 graphs in total, for each graph we will provide a small writeup to explain it.    
-*/
+
 const Visual = () => {
     return (
         <div>
             <div id='Title' className='flex-col mt-8 mx-8 space-y-2'>
                 <h1 className='font-light text-4xl'>Breast Cancer Prediction</h1>
                 <h2 className='font-light text-xl'>Group 4 Major Project - Autumn 2024</h2>
-                <hr className='my-4' />
+                <hr className='my-4'/>
             </div>
 
             <div id='Cancer Info' className='flex-col mt-8 mx-8 space-y-2'>
                 <h1 className='font-normal text-2xl'>What is Breast Cancer?</h1>
                 <p className='font-light text-xl'>{cancerInformation}</p>
-                
+                <br/>
+                <p className='font-normal text-xl'>In this project we have created an ML Model which predicts if a breast lump is cancerous or not on the basis of data that is gleaned from a mammograph.</p>
             </div>
 
             <div className='flex justify-center mt-8 mx-8'>
                 <div id='Graphs' className='flex-col space-y-2 w-3/4'>
-                    <img className=' h-auto rounded-lg m-2' src='/Chart1.png' alt='graph1' />
-                    <img className=' h-auto rounded-lg m-2' src='/Chart1.png' alt='graph1' />
-                    <img className=' h-auto rounded-lg m-2' src='/Chart1.png' alt='graph1' />
-                    <img className=' h-auto rounded-lg m-2' src='/Chart1.png' alt='graph1' />
+                    <img className=' h-auto rounded-lg m-2' src='/Graph5.png' alt='graph1' />
+                    <img className=' h-auto rounded-lg m-2' src='/Graph10.png' alt='graph1' />
+                    <img className=' h-auto rounded-lg m-2' src='/Graph11.png' alt='graph1' />
+                    <img className=' h-auto rounded-lg m-2' src='/Graph9.png' alt='graph1' />
+                    
+                    <div className='flex h-1/5 justify-center'>
+                    <img className=' h-auto rounded-lg m-2' src='/Graph8.png' alt='graph1' />
+                    <img className=' h-auto rounded-lg m-2' src='/Graph7.png' alt='graph1' />
+                    <img className=' h-auto rounded-lg m-2' src='/Graph4.png' alt='graph1' />
+                    
+                    </div>
+                    
+                    <img className=' h-auto rounded-lg m-2' src='/Graph6.png' alt='graph1' />
                 </div>
             </div>
             
--- src/components/info.js
+++ src/components/info.js
@@ -1,7 +1,6 @@
 const cancerInformation = `Breast cancer occurs almost entirely in women, but men can get breast cancer, too.
-Cancer starts when cells begin to grow out of control.
+Cancer starts when cells begin to grow out of control.\n
 It’s important to understand that most breast lumps are benign and not cancer (malignant). Non-cancer breast tumors are abnormal growths, but they do not spread outside of the breast. They are not life threatening, but some types of benign breast lumps can increase a woman's risk of getting breast cancer.
-In this project we have created an ML Model which predicts if a breast lump is cancerous or not on the basis of data that is gleaned from a mammograph.
-`;
+\n`;
 
 export default cancerInformation;
